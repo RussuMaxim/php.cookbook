d110ba9f2f26c00584187f94a4d22e3c8576c07f
Maxim <maximrussu@mail.ru>
2020-09-15 11:47:44 +0300

    Многострочные комментарии

--- exemplu1.14.php
+++ exemplu1.14.php
@@ -6,8 +6,13 @@ print  "<br/>";
 print 'Duck with Pea Shoots ' . "<br/>";
 print 'costs $9.50.' . "<br/>";
 print  "<br/>";
+// Знаки // или # можно употреблять в однострочных
+// комментариях. Комментарии начинаются со знаков
+// // или # в любом другом месте строки кода
 print 'Shark Fin Soup' . "<br/>";// Надеюсь, что это вкусно!
 print 'costs $25.00!';
+# Комментарии не начинаются со знаков // или #,
+# указываемых в символьной строке
 print  "<br/>";
 print 'http://www.example.com' . "<br/>";
 print 'http://www.example.com/menu.php#dinner' . "<br/>";
