cd258c7f496626ab778d0718debb8a6596bf8d0e
Maxim <maximrussu@mail.ru>
2020-09-15 11:38:59 +0300

    Однострочные комментарии, обозначаемые знаками // и #

---
+++ exemplu1.14.php
@@ -0,0 +1,13 @@
+<?php
+print "Smoked Fish Soup" . "<br/>";
+print 'cost $3.25.' . "<br/>";
+print  "<br/>";
+# ввести еще одно блюдо в меню
+print 'Duck with Pea Shoots ' . "<br/>";
+print 'costs $9.50.' . "<br/>";
+print  "<br/>";
+print 'Shark Fin Soup' . "<br/>";// Надеюсь, что это вкусно!
+print 'costs $25.00!';
+print  "<br/>";
+print 'http://www.example.com' . "<br/>";
+print 'http://www.example.com/menu.php#dinner' . "<br/>";
