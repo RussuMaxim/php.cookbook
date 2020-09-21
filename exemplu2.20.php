eda6cae597a7d39b5de0748fad810916840b1dcb
Maxim <maximrussu@mail.ru>
2020-09-21 15:58:07 +0300

    Инкрементирование и декрементирование

---
+++ exemplu2.20.php
@@ -0,0 +1,20 @@
+<?php
+$birthday = 2;
+$birthday = $birthday + 1;
+print $birthday;
+?><br/>
+<?php
+$birthday = 2;
+++$birthday;
+print $birthday;
+?> <br/>
+<?php
+$years_left = 3;
+$years_left = $years_left - 1;
+print $years_left;
+?><br/>
+<?php
+$years_left = 3;
+--$years_left;
+print $years_left;
+
